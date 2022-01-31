0d9d56fd1d669a24809c4fb7f43b7eafaf293712
Aydeniztr <88689203+Aydeniztr@users.noreply.github.com>
2022-02-01 01:30:35 +0300

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -13,10 +13,12 @@ want to install the required modules (y/n)
 
 '''
 read $choice
-if $choice == 'y':
-then echo '\033[35minstallation starded\033[0m'
-then pip install beautifulsoup4
-then pip install requests
-then echo 'installation done \033[31m!!!\033[0m'
-if $choice == 'n':
-then echo 'exiting...'
+
+
+if [ $numb = "1" ]
+then
+pip install beautifulsoup4
+pip install requests
+else
+fi
+fi
